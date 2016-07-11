3a09d14443a1d6c64c0e2927dff4194bf7008539
Slime Cuty <slimecuty@gmail.com>
2016-07-12 00:33:11 +0800

    Added RGB Selector and .gitignore

---
+++ RGB Selector/RGB Selector/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace RGB_Selector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(65, 200);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 26);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Text = "0, 0, 0";
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.Validating += new System.ComponentModel.CancelEventHandler(this.textBox2_Validating);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(65, 168);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 26);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "#000000";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(12, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "RGB:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(12, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "HEX:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(168, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "B:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(168, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "G:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(168, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "R:";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(201, 114);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(271, 45);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.TickFrequency = 5;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(201, 63);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(271, 45);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.TickFrequency = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(201, 12);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(271, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 300);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "Form1";
+            this.Text = "RGB Selector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RGB Selector/RGB Selector/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGB_Selector
+{
+    public partial class Form1 : Form
+    {
+        int r, g, b;  //Current color's RGB.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            r = g = b = 0;  //Initialize color is Black.
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            r = trackBar1.Value;  //R's TrackBar scroll change r value.
+            ReColor();  //Refresh display.
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            g = trackBar2.Value;  //G's TrackBar scroll change g value.
+            ReColor();  //Refresh display.
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            b = trackBar3.Value;  //B's TrackBar scroll change b value.
+            ReColor();  //Refresh display.
+        }
+        //textBox1 LostFocus triggers.
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            //Input Format: "#RRGGBB" or HTMLColor.
+            //ex: "#CCC" or "#CCCCCC" or "blue".
+            //  ("#CCC" equals "#CCCCCC")
+            try
+            {
+                Color c = ColorTranslator.FromHtml(textBox1.Text);  //Turn HTMLColor into Color.
+                //If textBox1.Text has the wrong format, go to Catch and skip the Change Color part.
+                //Change color.
+                r = c.R;
+                g = c.G;
+                b = c.B;
+            }
+            catch (Exception) { }
+            ReColor(); //Refresh display.
+        }
+        //textBox2 LostFocus triggers.
+        private void textBox2_Validating(object sender, CancelEventArgs e)
+        {
+            //Input Format: "R,G,B" or "R G B" or "R, G, B".
+            //ex: "204,204,204" or "204 204 204" or "204, 204, 204".
+            String[] str = textBox2.Text.Split(new String[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries);
+            if (str.Length == 3)  //If not 3 numbers, go to ReColor() and skip the Change Color part.
+            {
+                try
+                {
+                    int rr, gg, bb;  //Temporary variables.
+                    rr = Int32.Parse(str[0]);
+                    gg = Int32.Parse(str[1]);
+                    bb = Int32.Parse(str[2]);
+                    //If textBox2.Text has the wrong format, go to Catch and skip the Change Color part.
+                    if (rr >= 0 && gg >= 0 && bb >= 0 && rr < 256 && gg < 256 && bb < 256)  //between 0 and 255 is the correct value.
+                    {
+                        //Only change color after confirmed the format.
+                        r = rr;
+                        g = gg;
+                        b = bb;
+                    }
+                }
+                catch (Exception) { }
+            }
+            ReColor(); //Refresh display.
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Make Enter key triggers Change Color.
+            //Focus trackBar1 is to make textBox1 Lost Focus and triggers textBox1_Validating event.
+            if (e.KeyCode == Keys.Enter) trackBar1.Focus();
+        }
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Make Enter key triggers Change Color.
+            //Focus trackBar1 is to make textBox2 Lost Focus and triggers textBox2_Validating event.
+            if (e.KeyCode == Keys.Enter) trackBar1.Focus();
+        }
+        //Triggers when r, g, b changed.
+        void ReColor()
+        {
+            // Change color of pictureBox1.
+            pictureBox1.BackColor = Color.FromArgb(r, g, b);
+
+            //int.ToString("X2") Converts a number from Dec to Hex.
+            //Output: "#RRGGBB".
+            //ex: "#CCCCCC".
+            //If enters: "#ccc" force change to "#CCCCCC".
+            textBox1.Text = "#" + r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+
+            //Output: "R, G, B".
+            //ex: "204, 204, 204".
+            //If enters: "204 204 204" force change to "204, 204, 204".
+            textBox2.Text = r + ", " + g + ", " + b;
+
+            //Change Trackbar scroll block position.
+            trackBar1.Value = r;
+            trackBar2.Value = g;
+            trackBar3.Value = b;
+        }
+    }
+}
